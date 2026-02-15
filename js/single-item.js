6e1492a99d6563bac9c35b9223dcc0fedbe5fd9f
Aavash Tiwari <aavash190@gmail.com>
2026-02-15 11:05:24 +0545

    Code updated in single-item.js

--- js/single-item.js
+++ js/single-item.js
@@ -1,25 +1,40 @@
-// Create one list item element
-export function createSingleItem(item) {
+// Import the editCompleted function from app.js
+import { editCompleted } from "./app.js";
+
+// This function creates one grocery item element
+export default function createSingleItem(item) {
+  // Create a new div element
   const div = document.createElement("div");
-  div.className = "single-item";
 
-  const isCompleted = item.completed;
+  // Add class for styling
+  div.classList.add("single-item");
 
+  // If item is completed, add completed class
+  if (item.completed) {
+    div.classList.add("completed");
+  }
+
+  // Insert HTML structure inside div
   div.innerHTML = `
-    <input type="checkbox" ${isCompleted ? "checked" : ""} />
-
-    <p style="text-decoration: ${isCompleted ? "line-through" : "none"}">
-      ${item.name}
-    </p>
-
-    <button type="button" class="btn icon-btn edit-btn">
+    <input type="checkbox" ${item.completed ? "checked" : ""} />
+    <p>${item.name}</p>
+    <button class="edit-btn">
       <i class="fa-regular fa-pen-to-square"></i>
     </button>
-
-    <button type="button" class="btn icon-btn remove-btn">
+    <button class="remove-btn">
       <i class="fa-regular fa-trash-can"></i>
     </button>
   `;
 
+  // Select the checkbox inside this div
+  const checkbox = div.querySelector('input[type="checkbox"]');
+
+  // Add event listener to checkbox
+  checkbox.addEventListener("change", () => {
+    // When checkbox is changed, call editCompleted function
+    editCompleted(item.id);
+  });
+
+  // Return the completed div element
   return div;
 }
